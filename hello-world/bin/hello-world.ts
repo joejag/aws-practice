463e13f9cb9e34dfe1f3f54ba4ad0856b436386c
joe.wright <joe@joejag.com>
2020-05-09 20:40:20 +0100

    feat(hello-world): cdk sample-app

---
+++ hello-world/bin/hello-world.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core'
+import { HelloWorldStack } from '../lib/hello-world-stack'
+
+const app = new cdk.App()
+new HelloWorldStack(app, 'HelloWorldStack')
---
+++ hello-world/lib/hello-world-stack.ts
@@ -0,0 +1,12 @@
+import * as sns from '@aws-cdk/aws-sns';
+import * as subs from '@aws-cdk/aws-sns-subscriptions';
+import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+
+export class HelloWorldStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+   
+  } 
+}
