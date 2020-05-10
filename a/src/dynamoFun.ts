8ffaef62a4888f6ee789c3a01859eeca7c5f2d4c
joe.wright <joe@joejag.com>
2020-05-10 18:48:08 +0100

    feat(poll): allow using polls using only one vote

--- a/src/dynamoFun.ts
+++ a/src/dynamoFun.ts
@@ -3,20 +3,22 @@ import * as uuidv4 from "uuid/v4";
 
 AWS.config.update({ region: "eu-west-2" });
 const TABLE_NAME =
-  process.env.TABLE_NAME || "DynamoStack-TableCD117FA1-Z4O6HSYY9B7W";
+  process.env.TABLE_NAME || "DynamoStack-Votes58095409-1G0VD3PB0GPL";
+const POLL_ID = "e6b46aff-d039-476b-b4ab-364c66dc6579";
 
 const db = new AWS.DynamoDB.DocumentClient();
 
+// create poll
 var param = {
   TableName: TABLE_NAME,
   Item: {
-    id: uuidv4(),
+    ID: uuidv4(),
     TITLE: "Who should be primeminster?",
     OPTIONS: ["Boris", "Nicola", "Bob"],
     VOTE_TYPE: "LIMIT",
     LIMIT: 1,
-    ANON: false,
-    OPEN: true,
+    IS_ANON: false,
+    IS_OPEN: true,
     CREATED_AT: "date",
     CREATED_BY: "joewright",
     VOTES: {},
@@ -34,9 +36,10 @@ var param = {
 //   .promise()
 //   .then((d) => console.log(d.Items[0]));
 
+// add vote
 db.update({
   TableName: TABLE_NAME,
-  Key: { id: "293b1de1-d14d-4816-8a5f-43550a8f21b0" },
+  Key: { ID: POLL_ID },
   ReturnValues: "ALL_NEW",
   UpdateExpression: "ADD VOTES.#poller :OPTION",
   ExpressionAttributeNames: {
@@ -47,12 +50,12 @@ db.update({
   },
 })
   .promise()
-  .then(console.log)
   .catch(console.log);
 
+// add vote
 db.update({
   TableName: TABLE_NAME,
-  Key: { id: "293b1de1-d14d-4816-8a5f-43550a8f21b0" },
+  Key: { ID: POLL_ID },
   ReturnValues: "ALL_NEW",
   UpdateExpression: "ADD VOTES.#poller :OPTION",
   ExpressionAttributeNames: {
@@ -63,12 +66,12 @@ db.update({
   },
 })
   .promise()
-  .then(console.log)
   .catch(console.log);
 
+// remove vote
 db.update({
   TableName: TABLE_NAME,
-  Key: { id: "293b1de1-d14d-4816-8a5f-43550a8f21b0" },
+  Key: { ID: POLL_ID },
   ReturnValues: "ALL_NEW",
   UpdateExpression: "DELETE VOTES.#poller :OPTION",
   ExpressionAttributeNames: {
@@ -79,5 +82,32 @@ db.update({
   },
 })
   .promise()
-  .then(console.log)
+  .catch(console.log);
+
+db.update({
+  TableName: TABLE_NAME,
+  Key: { ID: POLL_ID },
+  ReturnValues: "ALL_NEW",
+  UpdateExpression: "SET IS_OPEN = :MODE",
+  ExpressionAttributeValues: {
+    ":MODE": false,
+  },
+})
+  .promise()
+  .catch(console.log);
+
+// 1 VOTE ALLOWED - clear existing votes
+db.update({
+  TableName: TABLE_NAME,
+  Key: { ID: POLL_ID },
+  ReturnValues: "ALL_NEW",
+  UpdateExpression: "SET VOTES.#poller = :OPTION",
+  ExpressionAttributeNames: {
+    "#poller": "george",
+  },
+  ExpressionAttributeValues: {
+    ":OPTION": db.createSet(["Tom"]),
+  },
+})
+  .promise()
   .catch(console.log);
